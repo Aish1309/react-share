9fec0e8a7f0921d56f8f0e74658fa1a1831ac4b8
aish13090 <saishvarya24ece@srishakthi.ac.in>
2024-09-05 16:06:06 +0530

    h

--- dist/App.js
+++ dist/App.js
@@ -12,14 +12,11 @@ var _jsxRuntime = require("react/jsx-runtime");
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
 function App() {
   return /*#__PURE__*/(0, _jsxRuntime.jsx)(_reactRouterDom.BrowserRouter, {
-    children: /*#__PURE__*/(0, _jsxRuntime.jsxs)(_reactRouterDom.Routes, {
-      children: [/*#__PURE__*/(0, _jsxRuntime.jsx)(_reactRouterDom.Route, {
+    children: /*#__PURE__*/(0, _jsxRuntime.jsx)(_reactRouterDom.Routes, {
+      children: /*#__PURE__*/(0, _jsxRuntime.jsx)(_reactRouterDom.Route, {
         path: "/",
         element: /*#__PURE__*/(0, _jsxRuntime.jsx)(_LandingPage.LandingPage, {})
-      }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_reactRouterDom.Route, {
-        path: "/login",
-        element: /*#__PURE__*/(0, _jsxRuntime.jsx)(_Login.LoginPage, {})
-      })]
+      })
     })
   });
 }
